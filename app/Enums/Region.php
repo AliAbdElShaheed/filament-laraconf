51955c30e0853654d1a1e26a307b4ad2ff51210f
Ali Mohammed <engineer.ali.mohammed@fcai.usc.edu.eg>
2025-05-24 02:34:23 +0300

    Adding a Region Enum && Adding region field to venue table also

---
+++ app/Enums/Region.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Enums;
+
+enum Region : string
+{
+    case NorthAmerica = 'North America';
+    case SouthAmerica = 'South America';
+    case Europe = 'Europe';
+    case Asia = 'Asia';
+    case Africa = 'Africa';
+    case Australia = 'Australia';
+    case Antarctica = 'Antarctica';
+
+    case MiddleEast = 'Middle East';
+
+    case Online = 'Online';
+
+} // end enum
